f774cbaeddf8d71b80cb27a92a28e1cb7ad8947d
alexia11100 <alexiasacerdotedeoliveira@gmail.com>
2024-01-24 14:02:52 -0300

    Create some functions

--- src/test/java/TestCase1.java
+++ src/test/java/TestCase1.java
@@ -1,9 +1,12 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 // História do Usuário #1:
 // Como um cliente cadastrado no https://automationexercise.com/
@@ -14,48 +17,65 @@ import org.openqa.selenium.chrome.ChromeDriver;
 // Beautiful Peacock Blue Cotton Linen Saree - 2 itens
 // Men Tshirt - 1
 public class TestCase1 {
+    WebDriver driver = new ChromeDriver();
+
     @Test
     public void testPurchaseMultipleItems() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\alexia.oliveira\\rest assured VemSer\\vs12-qa\\modulo-04-api\\restassured\\QA-Challenge-UI\\chromedriver-win64\\chromedriver.exe");
-
-        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         // Usuário acessa a conta
         driver.get("https://automationexercise.com/login");
-
-        WebElement emailField = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[2]"));
-        WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]"));
-        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button"));
-
-        emailField.sendKeys("teste87@teste.com");
-        passwordField.sendKeys("teste123");
-        loginButton.click();
-
-        WebElement loggedInElement = driver.findElement(By.xpath("//a[contains(text(), 'Logged in as')]"));
+        login("teste87@teste.com", "teste123");
+        driver.findElement(By.xpath("//a[contains(text(), 'Logged in as')]"));
 
         // Usuário acessa a página de produtos
         driver.get("https://automationexercise.com/products");
 
+        searchProduct("Stylish Dress");
+        WebElement viewProductButton = driver.findElement(By.xpath("/html/body/section[2]/div[1]/div/div[2]/div/div[2]/div/div[2]/ul/li/a"));
+        viewProductButton.click();
+        closeAd();
+
+        WebElement productTitle = driver.findElement(By.xpath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/h2"));
+        Assert.assertEquals("Stylish Dress", productTitle.getText());
+
+        WebElement productAmount = driver.findElement(By.xpath("//*[@id=\"quantity\"]"));
+        productAmount.sendKeys(Keys.CONTROL, "a");
+        productAmount.sendKeys("3");
+        Assert.assertEquals("3", productAmount.getAttribute("value"));
+
+
+        //searchProduct("Beautiful Peacock Blue Cotton Linen Saree");
+        //searchProduct("Men Tshirt");
+
+    }
+
+    public void searchProduct(String name){
         WebElement searchField = driver.findElement(By.xpath("//*[@id=\"search_product\"]"));
         WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"submit_search\"]"));
 
         searchField.sendKeys(Keys.CONTROL, "a");
-        searchField.sendKeys("Stylish Dress");
+        searchField.sendKeys(name);
         searchButton.click();
+    }
 
-        searchField = driver.findElement(By.xpath("//*[@id=\"search_product\"]"));
-        searchButton = driver.findElement(By.xpath("//*[@id=\"submit_search\"]"));
+    public void login(String email, String password){
+        WebElement emailField = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[2]"));
+        WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button"));
 
-        searchField.sendKeys(Keys.CONTROL, "a");
-        searchField.sendKeys("Beautiful Peacock Blue Cotton Linen Saree");
-        searchButton.click();
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
 
-        searchField = driver.findElement(By.xpath("//*[@id=\"search_product\"]"));
-        searchButton = driver.findElement(By.xpath("//*[@id=\"submit_search\"]"));
-
-        searchField.sendKeys(Keys.CONTROL, "a");
-        searchField.sendKeys("Men Tshirt");
-        searchButton.click();
+    public void closeAd(){
+        WebElement frameAd = driver.findElement(By.xpath("/html/ins/div/iframe"));
+        driver.switchTo().frame(frameAd);
+        WebElement closeButton = driver.findElement(By.xpath("//*[@id=\"dismiss-button\"]"));
+        closeButton.click();
+        driver.switchTo().defaultContent();
     }
 }
